59a40cc60b247949f4793f51e2edd7387cdf123e
Pansystar <wh.bt3.p@gmail.com>
2021-02-14 09:57:21 +0900

    連想配列を利用して処理を高速化

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ itemControl = ItemControl()
 titles = gw.getAllTitles()
 handle = None
 
+## 雀魂を探す
 for item in titles:
     if("雀魂" in item):
         handle = ctypes.windll.user32.FindWindowW(0, item)
@@ -38,42 +39,58 @@ if(handle != None):
     julius = Julius()
     hai_list = list()
 
-    for i in range(10000000):
+    special_command_dict = {
+        "ツモ切り": game.CutTheTsumo,
+        "スキップ": game.Skip,
+        "チー": game.Chi,
+        "ポン": game.Pon,
+        "ロン": game.Pon,
+        "ツモ": game.Pon,
+        "立直": game.Pon,
+    }
+
+    while True:
         try:
             hai_list.clear()
             time.sleep(1.0)
             game.Update()
             julius.Update()
-
-            img_list = game.CaptureToImage()
-            
-            for item in img_list:              
-                # イメージインスタンス
-                img = np.asarray(item)
-                # データをバッファに格納
-                result = itemControl.analyze(img)
-                hai_list.append(result)
                 
             if julius.text != "":
-                for i in range(len(hai_list)):
-                    if hai_list[i].name == julius.text:
-                        game.CutTehai(i)
-                
-                if "ツモ切り" == julius.text:
-                    game.CutTheTsumo()
-                if "スキップ" == julius.text:
-                    game.Skip()
-                if "チー" == julius.text:
-                    game.Chi()
-                if "ポン" == julius.text:
-                    game.Pon()
-                if "ロン" == julius.text:
-                    game.Pon()
-                if "ツモ" == julius.text:
-                    game.Pon()
-                if "立直" == julius.text:
-                    game.Pon()
+
+                if julius.text in special_command_dict:
+                    special_command_dict[julius.text]()
+                else:
+                    img_list = game.CaptureToImage()
+                    
+                    for item in img_list:
+                        # イメージインスタンス
+                        img = np.asarray(item)
+                        # データをバッファに格納
+                        result = itemControl.analyze(img)
+                        hai_list.append(result)
+
+                    for i in range(len(hai_list)):
+                        if hai_list[i].name == julius.text:
+                            game.CutTehai(i)
+                    
                 julius.text = ""
+
+                
+                # if "ツモ切り" == julius.text:
+                #     game.CutTheTsumo()
+                # if "スキップ" == julius.text:
+                #     game.Skip()
+                # if "チー" == julius.text:
+                #     game.Chi()
+                # if "ポン" == julius.text:
+                #     game.Pon()
+                # if "ロン" == julius.text:
+                #     game.Pon()
+                # if "ツモ" == julius.text:
+                #     game.Pon()
+                # if "立直" == julius.text:
+                #     game.Pon()
         except:
             julius.text = ""
         
